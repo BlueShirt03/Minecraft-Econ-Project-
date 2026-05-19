5cee660d6de8274acc861060599ea96e51e89ca8
BlueShirt03 <157056676+BlueShirt03@users.noreply.github.com>
2026-05-19 14:45:00 -0500

    I was able to create a connection file so that why I am able to interacte with the .env file

---
+++ bot/connection.py
@@ -0,0 +1,28 @@
+import os 
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
+
+def get_connection():
+    try:
+        connection = psycopg2.connect(
+        host = DB_HOST,
+        database  = DB_NAME,
+        user = DB_USER,
+        password = DB_PASSWORD,
+        port = DB_PORT
+
+    )
+    except ValueError as e:
+        print(f"Error connecting to database: {e}")
+        return None
+    return connection
+    
