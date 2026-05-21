57ed7425031b1b8309db944b8ae373a13dae8bee
BlueShirt03 <157056676+BlueShirt03@users.noreply.github.com>
2026-05-21 11:39:25 -0500

    I was creating a function that deletes a player based on the Minecraft user name, I will be testing it later.

--- bot/repositories/player_repository.py
+++ bot/repositories/player_repository.py
@@ -27,8 +27,8 @@ class PlayerRepository:
         finally:
             cursor.close()
             connection.close()
-    
-    def delete_player(self, discord_id, minecraft_username):
+      
+    def delete_player(self,minecraft_username):
         connection = get_connection()
 
         if connection is None:
@@ -38,21 +38,37 @@ class PlayerRepository:
             cursor = connection.cursor()
 
             cursor.execute(
-                """DELETE FROM player
-                WHERE player = discord_id, 
-                """
+               """Delete from player
+               WHERE minecraft_username = %s
+               """, (minecraft_username)
             )
-
+            connection.commit()
+            return True
+        except Exception as e:
+            print(f"Error creating player: {e}")
+            return False
+        
+        finally:
+            cursor.close()
+            connection.close()
+    
 if __name__ == "__main__":
      player_repository = PlayerRepository()
 
-     success = player_repository.create_player(
+     success1 = player_repository.create_player(
          discord_id = 123456789, 
          minecraft_username = "PotatoLover25"
      )
 
-     if success:
-         print("Player created successfully")
-     else:
-         print("Player was not able to be created")
-    
+     success2 = player_repository.create_player(
+         discord_id = 8374873, 
+         minecraft_username = "BlockMaster29"
+     )
+
+     success3 = player_repository.create_player(
+         discord_id = 17683640, 
+         minecraft_username = "YoMama67"
+     )
+     print(success1)   
+     print(success2)   
+     print(success3)   
