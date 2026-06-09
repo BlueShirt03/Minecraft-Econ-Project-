00155baacd141fef14f7e1abf5e866c04bc0c78a
BlueShirt03 <157056676+BlueShirt03@users.noreply.github.com>
2026-06-08 22:10:45 -0500

    I started working on the get player balance funtion. However, I am running into trouble right now, I will figure it out tomorrow.

--- bot/services/economy_service.py
+++ bot/services/economy_service.py
@@ -14,11 +14,26 @@ class EconomySerivce():
             self.player_repository.create_player(discord_id, minecraft_username)
             return True
 
+    def get_player_balance(self,minecraft_username):
+
+
+        if self.player_repository.player_exist(minecraft_username):
+            return False
+        else:
+            balance = self.player_repository.get_balance(minecraft_username)
+            return balance
+
+
+
+
+
 economy_service = EconomySerivce()
 
 success = economy_service.register_player(discord_id = 987654321, minecraft_username = "BubbleGum25")
 
-if success:
-    print("Player was able to be registered.")
+success2 = economy_service.get_player_balance(minecraft_username="BlockMaster29")
+
+if success2:
+    print("Could find balance.")
 else:
-    print("Player was a not able to be registered.")
+    print("Could not find balance.")
