abb6fa37700735fcdcb9af343182769ec9991cb6
BlueShirt03 <157056676+BlueShirt03@users.noreply.github.com>
2026-06-08 21:40:38 -0500

    I was able to test the register_player function and it worked. However, I did get an error the first time because in first part of the if statemnet I had the wrong parameter.

--- bot/services/economy_service.py
+++ bot/services/economy_service.py
@@ -8,9 +8,17 @@ class EconomySerivce():
     
     def register_player(self,discord_id, minecraft_username):
 
-        if self.player_repository.player_exist(discord_id):
+        if self.player_repository.player_exist(minecraft_username):
             return False
         else:
             self.player_repository.create_player(discord_id, minecraft_username)
             return True
 
+economy_service = EconomySerivce()
+
+success = economy_service.register_player(discord_id = 987654321, minecraft_username = "BubbleGum25")
+
+if success:
+    print("Player was able to be registered.")
+else:
+    print("Player was a not able to be registered.")
